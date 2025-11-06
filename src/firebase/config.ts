c9126fd9dcfda6c2c409583307bfdd905ba8053e
عبد السلام <abozbere2001@gmail.com>
2025-11-06 14:05:08 +0000

    انا الذي اعرفه هو هذا المشروع nabd-d71ab.   لكن لا اعرف متى انت قمت بتغي

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
+
 export const firebaseConfig = {
-  "projectId": "studio-3417145591-24d0a",
-  "appId": "1:325356206087:web:0e81db85d0f41df9b652a2",
-  "apiKey": "AIzaSyCqxDR5-mmX0t9AZ3wfUTc5GT1MT_FiMyY",
-  "authDomain": "studio-3417145591-24d0a.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "325356206087"
+  "projectId": "nabd-d71ab",
+  "appId": "1:665814234557:web:2510c598285a2287661131",
+  "apiKey": "AIzaSyAz-bV3_E4zcr9qWMnWT_4rA-2JqsSy_kw",
+  "authDomain": "nabd-d71ab.firebaseapp.com",
+  "storageBucket": "nabd-d71ab.appspot.com",
+  "messagingSenderId": "665814234557"
 };
