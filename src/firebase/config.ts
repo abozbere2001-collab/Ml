47f02a3282080eaf4c21f719fe28805b98e0ba8d
عبد السلام <abozbere2001@gmail.com>
2025-11-06 14:03:18 +0000

    FirebaseError: Firebase: Error (auth/operation-not-allowed).     at crea

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "goal-stack-finly13-71597-9ee57",
+  "projectId": "studio-3417145591-24d0a",
   "appId": "1:325356206087:web:0e81db85d0f41df9b652a2",
   "apiKey": "AIzaSyCqxDR5-mmX0t9AZ3wfUTc5GT1MT_FiMyY",
-  "authDomain": "goal-stack-finly13-71597-9ee57.firebaseapp.com",
+  "authDomain": "studio-3417145591-24d0a.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "325356206087"
 };
