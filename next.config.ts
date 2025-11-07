463919340841bc7a3e911db45416d47c2bf6a6b0
عبد السلام <abozbere2001@gmail.com>
2025-11-07 09:07:55 +0000

    .

--- next.config.ts
+++ next.config.ts
@@ -13,7 +13,7 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
+  disable: isProd,
 });
 
 const nextConfig: NextConfig = {
