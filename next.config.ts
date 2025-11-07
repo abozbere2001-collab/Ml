72567f6bb5b5136e09532d8e2642abf812a53139
عبد السلام <abozbere2001@gmail.com>
2025-11-07 09:30:18 +0000

    Skip to content Navigation Menu Ml
    Code
    Issues
    Pull requests
    Build and

--- next.config.ts
+++ next.config.ts
@@ -14,7 +14,6 @@ const withPWA = require('next-pwa')({
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
-  basePath: basePath,
 });
 
 const nextConfig: NextConfig = {
