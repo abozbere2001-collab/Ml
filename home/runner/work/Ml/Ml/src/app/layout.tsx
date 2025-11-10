730e7f974c7fd6ab42b3084d32e7575f41ede8fb
عبد السلام <abozbere2001@gmail.com>
2025-11-10 14:39:42 +0000

    Skip to content Navigation Menu Ml
    Code
    Issues
    Pull requests
    Build and

--- home/runner/work/Ml/Ml/src/app/layout.tsx
+++ home/runner/work/Ml/Ml/src/app/layout.tsx
@@ -3,7 +3,7 @@ import type {Metadata} from 'next';
 import '@/app/globals.css';
 import { Toaster } from "@/components/ui/toaster"
 import { ThemeProvider } from '@/components/theme-provider';
-import { Cairo } from 'next/font/google';
+import { Inter } from 'next/font/google';
 import { FirebaseClientProvider } from '@/firebase';
 
 const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
@@ -19,10 +19,9 @@ export const metadata: Metadata = {
   },
 };
 
-const cairo = Cairo({
-  subsets: ['arabic'],
-  weight: ['400', '700'],
-  variable: '--font-cairo',
+const inter = Inter({
+  subsets: ['arabic', 'latin'],
+  variable: '--font-inter',
 });
 
 export default function RootLayout({
@@ -44,7 +43,7 @@ export default function RootLayout({
           <link rel="manifest" href={`${basePath}/manifest.json`} />
           <link rel="apple-touch-icon" href={`${basePath}/icon-192x192.png`} />
         </head>
-        <body className={`${cairo.variable} font-body antialiased`}>
+        <body className={`${inter.variable} font-body antialiased`}>
             <ThemeProvider
                 attribute="class"
                 defaultTheme="system"
